74e949c86b26ab5191962c6e83b5de11cb794111
jotapeastorga <jpastorga@falabella.cl>
2019-11-24 20:42:09 -0300

    connection mongo

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://zeus:yUVhXH32p7npPAJ@boticacomunitaria-himc8.gcp.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+
+const uri = "mongodb+srv://zeus:yUVhXH32p7npPAJ@boticacomunitaria-himc8.gcp.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("boticavictoria-stag").collection("departamentos");
+
+   collection.find({}).toArray((error, result) => {
+    if(error) {
+        return response.status(500).send(error);
+    }
+    console.log(result);
 });
+
+
+   client.close();
+});
+
